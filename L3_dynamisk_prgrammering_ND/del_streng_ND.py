fa0226b2f0a636174ddcaa23f086dc5c67eebc6d
MadeleineHarbom <madeleine.harbom@gmail.com>
2020-03-10 21:53:18 +0100

    cfhg

--- L3_dynamisk_prgrammering_ND/del_streng_ND.py
+++ L3_dynamisk_prgrammering_ND/del_streng_ND.py
@@ -1,39 +1,61 @@
 
 
 def print_alle_delstrenge(thestring):
-    #thedict = thestring.toCharArray()
-    thearray = list(thestring)
-    print(thearray)
-    for i in range(0, len(thearray)):
-        print("index: ")
-        print("i: " + str(i))
-        if i < len(thearray):
-            for j in range(i, len(thearray)):
-                print("j: " + str(j))
-                print(thearray[i:j+1])
+    mylist = list(thestring)
+    for i in range(0, len(mylist)):
+        templist = []
+        for j in range(i, len(mylist)):
+            templist.append(mylist[j])
+        print(templist)
 
 
-def find_laengste_delstreng(string1, string2):
+
+def find_laengeste_delstreng_opgave2_for_loops(string1, string2):
     thearray1 = list(string1)
     thearray2 = list(string2)
     longeststring = ''
-    print(thearray1)
-    print(thearray2)
-    for i in range(0, len(thearray1)):
-        if j < len(thearray2):
-            if i == j:
-                tempstring = ''
-                print('Match')
+    for i in range(0, len(string1)):
+        for j in range(0, len(string2)):
+            if string1[i] == string2[j]:
+                print('match: ' + string1[i] + ' og ' + string2[j])
+                delstring = ''
                 add = 0
-                while thearray1[i+add] == thearray2[j+add]:
-                    tempstring.append(thearray1[i+add])
-                if len(tempstring) > len(longeststring):
-                    longeststring = tempstring
+                while i+add < len(string1) and j+add < len(string2):
+                    if string1[i+add] == string2[j+add]:
+                        delstring += string1[i+add]
+                        add += 1
+                    else:
+                        print('immaouto break')
+
+                        break
+                if len(delstring) > len(longeststring):
+                    longeststring = delstring
     return longeststring
 
+def find_laengste_delstreng_opgave3_matrix(string1, string2):
+    matrixx = create_matrix(len(string1) + 1, len(string2) + 1)
+    print(matrixx)
+    for i in range(1, len(string1)):
+        for j in range(1, len(string2)):
+            if string1[i] == string2[j]:
+                matrixx[i][j] = matrixx[i-1][j-1] + 1
+    print(matrixx)
+
+def create_matrix(m, n):
+    return [[0 for y in range(m)] for x in range(n)]
+
+#TODO print matrixen
+def print_matrix(matrixx):
+    return 0
 
 
 
+#print_alle_delstrenge('Made')
+#print(find_laengeste_delstreng_opgave2('Made', 'Make'))
+print(find_laengeste_delstreng_opgave2_for_loops('piskeris', 'malerisk'))
+print('Tidskomplesiteten for find længste delstren er...') #TODO
+find_laengste_delstreng_opgave3_matrix('piskeris', 'malerisk')
 
-
-print_alle_delstrenge("Made")
+#TODO få den længste delstreng fra matrix læøsningen
+#TODO Hvad er tidskompleksiteten på matrix?
+#TODO Hvorfor er dette dynamisk programmering
