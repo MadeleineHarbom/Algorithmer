aaa2f4dd19e6418e5b865731e35bdb9d3173b3dc
MadeleineHarbom <madeleine.harbom@gmail.com>
2020-05-12 14:36:38 +0200

    Couch

--- L11_pattern_matching/boyerMoore.py
+++ L11_pattern_matching/boyerMoore.py
@@ -1,3 +1,28 @@
+from L11_pattern_matching.text import *
 
 def find_boyer_moore(text, pattern):
-    n, m = len(text), len(pattern)
+    t, p = len(text), len(pattern)
+    if p == 0: #hvis pattern er tomt
+        return 0
+    last = {}
+    for k in range(p):
+        last[pattern[k]] = k #lavet et array med bogstaverne i pattern som key, og k(int) som value
+    i = p-1
+    k = p-1
+    while i < t:
+        print('Checking ' + text[i] + ' at text-index ' + str(i) + 'against ' + pattern[k] + ' at pattern-index ' + str(k))
+        if text[i] == pattern[k]: # er det en match
+            if k == 0:
+                return i
+            else:
+                i -= 1
+                k -= 1 #Her begynder k at ticke ned
+        else:
+            j = last.get(text[i], -1) #hvad sker her?
+            i += p - min(k, j + 1)
+            k = p - 1 #sikrer at k er den samme, hvis ikke patern er fundet
+    return -1
+
+
+print(find_boyer_moore(bran, 'morning'))
+print(find_boyer_moore(dna, 'CCTTTTGC'))
---
+++ L11_pattern_matching/text.py
@@ -0,0 +1,155 @@
+dna = 'GCTTCTGCTACCTTTGCGCGCGCGCGGAA'
+
+bran = 'BRAN' \
+       'The morning had dawned clear and cold, with a crispness that hinted at the end ofsummer. They set forth at ' \
+       'daybreak to see a man beheaded, twenty in all, and Bran rodeamong them, nervous with excitement. This was the ' \
+       'first time he had been deemed oldenough to go with his lord father and his brothers to see the kingâ€™s justice ' \
+       'done. It wasthe ninth year of summer, and the seventh of Branâ€™s life.The man had been taken outside a small ' \
+       'holdfast in the hills. Robb thought he was awildling, his sword sworn to Mance Rayder, the King-beyond-the-Wall.' \
+       ' It made Branâ€™sskin prickle to think of it. He remembered the hearth tales Old Nan told them. Thewildlings' \
+       ' were cruel men, she said, slavers and slayers and thieves. They consorted withgiants and ghouls, stole girl ' \
+       'children in the dead of night, and drank blood from polishedhorns. And their women lay with the Others in the ' \
+       'Long Night to sire terrible halfhuman children.But the man they found bound hand and foot to the holdfast wall ' \
+       'awaiting the kingâ€™sjustice was old and scrawny, not much taller than Robb. He had lost both ears and afinger ' \
+       'to frostbite, and he dressed all in black, the same as a brother of the Nightâ€™sWatch, except that his furs were ' \
+       'ragged and greasy.The breath of man and horse mingled, steaming, in the cold morning air as his lordfather had the' \
+       ' man cut down from the wall and dragged before them. Robb and Jon sattall and still on their horses, with Bran ' \
+       'between them on his pony, trying to seem olderthan seven, trying to pretend that heâ€™d seen all this before.' \
+       ' A faint wind blew through theholdfast gate. Over their heads flapped the banner of the Starks of Winterfell: ' \
+       'a greydirewolf racing across an ice-white field.Branâ€™s father sat solemnly on his horse, long brown hair ' \
+       'stirring in the wind. His closelytrimmed beard was shot with white, making him look older than his thirty-five ' \
+       'years. Hehad a grim cast to his grey eyes this day, and he seemed not at all the man who would sitbefore the ' \
+       'fire in the evening and talk softly of the age of heroes and the children of theforest. He had taken off ' \
+       'Fatherâ€™s face, Bran thought, and donned the face of Lord Starkof Winterfell.There were questions asked and ' \
+       'answers given there in the chill of morning, but afterward Bran could not recall much of what had been said. ' \
+       'Finally his lord father gavea command, and two of his guardsmen dragged the ragged man to the ironwood stumpin' \
+       ' the center of the square. They forced his head down onto the hard black wood. LordEddard Stark dismounted and' \
+       ' his ward Theon Greyjoy brought forth the sword. â€œIce,â€that sword was called. It was as wide across as a ' \
+       'manâ€™s hand, and taller even than Robb.The blade was Valyrian steel, spell-forged and dark as smoke. Nothing' \
+       ' held an edge likeValyrian steel.His father peeled off his gloves and handed them to Jory Cassel, the captain' \
+       ' of hishousehold guard. He took hold of Ice with both hands and said, â€œIn the name of Robertof the House ' \
+       'Baratheon, the First of his Name, King of the Andals and the Rhoynar andthe First Men, Lord of the Seven' \
+       ' Kingdoms and Protector of the Realm, by the word ofEddard of the House Stark, Lord of Winterfell and Warden' \
+       ' of the North, I do sentenceyou to die.â€ He lifted the greatsword high above his head.Branâ€™s bastard brother' \
+       ' Jon Snow moved closer. â€œKeep the pony well in hand,â€ hewhispered. â€œAnd donâ€™t look away. Father will' \
+       ' know if you do.â€Bran kept his pony well in hand, and did not look away.His father took off the manâ€™s head' \
+       ' with a single sure stroke. Blood sprayed out across thesnow, as red as surnmerwine. One of the horses reared ' \
+       'and had to be restrained to keepfrom bolting. Bran could not take his eyes off the blood. The snows around the' \
+       ' stumpdrank it eagerly, reddening as he watched.The head bounced off a thick root and rolled. It came up near ' \
+       'Greyjoyâ€™s feet. Theon wasa lean, dark youth of nineteen who found everything amusing. He laughed, put his' \
+       ' booton the head, and kicked it away.â€œAss,â€ Jon muttered, low enough so Greyjoy did not hear. He put a ' \
+       'hand on Branâ€™sshoulder, and Bran looked over at his bastard brother. â€œYou did well,â€Jon told him ' \
+       'solemnly. Jon was fourteen, an old hand at justice.It seemed colder on the long ride back to Winterfell, though ' \
+       'the wind had died by thenand the sun was higher in the sky. Bran rode with his brothers, well ahead of the' \
+       ' mainparty, his pony struggling hard to keep up with their horses.â€œThe deserter died bravely,â€ Robb said. ' \
+       'He was big and broad and growing every day,with his motherâ€™s coloring, the fair skin, red-brown hair, and' \
+       ' blue eyes of the Tullys of Riverrun. â€œHe had courage, at the least.â€â€œNo,â€ Jon Snow said quietly. ' \
+       'â€œIt was not courage. This one was dead of fear. You couldsee it in his eyes, Stark.â€ Jonâ€™s eyes were a ' \
+       'grey so dark they seemed almost black, butthere was little they did not see. He was of an age with Robb, but ' \
+       'they did not look alike.Jon was slender where Robb was muscular, dark where Robb was fair, graceful andquick ' \
+       'where his half brother was strong and fast.Robb was not impressed. â€œThe Others take his eyes,â€ he swore. ' \
+       'â€œHe died well. Race youto the bridge?â€â€œDone,â€ Jon said, kicking his horse forward. Robb cursed and' \
+       ' followed, and theygalloped off down the trail, Robb laughing and hooting, Jon silent and intent. The hooves ' \
+       'of their horses kicked up showers of snow as they went.Bran did not try to follow. His pony could not keep up.' \
+       ' He had seen the ragged manâ€™seyes, and he was thinking of them now. After a while, the sound of Robbâ€™s ' \
+       'laughterreceded, and the woods grew silent again.So deep in thought was he that he never heard the rest of the ' \
+       'party until his fathermoved up to ride beside him. â€œAre you well, Bran?â€ he asked, not unkindly.â€œYes, ' \
+       'Father,â€ Bran told him. He looked up. Wrapped in his furs and leathers, mountedon his great warhorse, his ' \
+       'lord father loomed over him like a giant. â€œRobb says the mandied bravely, but Jon says he was afraid.' \
+       'â€â€œWhat do you think?â€ his father asked.Bran thought about it. â€œCan a man still be brave if heâ€™s ' \
+       'afraid?â€â€œThat is the only time a man can be brave,â€ his father told him. â€œDo you understand whyI did' \
+       ' it?â€â€œHe was a wildling,â€ Bran said. â€œThey carry off women and sell them to the Others.â€His lord ' \
+       'father smiled. â€œOld Nan has been telling you stories again. In truth, the manwas an oathbreaker, a deserter ' \
+       'from the Nightâ€™s Watch. No man is more dangerous. Thedeserter knows his life is forfeit if he is taken, so he' \
+       ' will not flinch from any crime, nomatter how vile. But you mistake me. The question was not why the man had to ' \
+       'die, butwhy I must do it.â€Bran had no answer for that. â€œKing Robert has a headsman,â€ he said, ' \
+       'uncertainly.â€œHe does,â€ his father admitted. â€œAs did the Targaryen kings before him. Yet our way isthe ' \
+       'older way. The blood of the First Men still flows in the veins of the Starks, and we hold to the belief that' \
+       ' the man who passes the sentence should swing the sword. If youwould take a manâ€™s life, you owe it to him to ' \
+       'look into his eyes and hear his final words.And if you cannot bear to do that, then perhaps the man does not ' \
+       'deserve to die.â€œOne day, Bran, you will be Robbâ€™s bannerman, holding a keep of your own for yourbrother and' \
+       ' your king, and justice will fall to you. When that day comes, you must takeno pleasure in the task, but neither' \
+       ' must you look away. A ruler who hides behind paidexecutioners soon forgets what death is.â€That was when Jon ' \
+       'reappeared on the crest of the hill before them. He waved andshouted down at them. â€œFather, Bran, come quickly,' \
+       ' see what Robb has found!â€ Thenhe was gone again.Jory rode up beside them. â€œTrouble, my lord?â€â€œBeyond a ' \
+       'doubt,â€ his lord father said. â€œCome, let us see what mischief my sons haverooted out now.â€ He sent his' \
+       ' horse into a trot. Jory and Bran and the rest came after.They found Robb on the riverbank north of the bridge,' \
+       ' with Jon still mounted besidehim. The late summer snows had been heavy this moonturn. Robb stood knee-deep ' \
+       'inwhite, his hood pulled back so the sun shone in his hair. He was cradling something inhis arm, while the boys' \
+       ' talked in hushed, excited voices.The riders picked their way carefully through the drifts, groping for solid ' \
+       'footing on thehidden, uneven ground. Jory Cassel and Theon Greyjoy were the first to reach the boys.Greyjoy was ' \
+       'laughing and joking as he rode. Bran heard the breath go out of him.â€œGods!â€ he exclaimed, struggling to keep' \
+       ' control of his horse as he reached for his sword.Joryâ€™s sword was already out. â€œRobb, get away from it!â€' \
+       ' he called as his horse rearedunder him.Robb grinned and looked up from the bundle in his arms. â€œShe canâ€™t' \
+       ' hurt you,â€ he said.â€œSheâ€™s dead, Jory.â€Bran was afire with curiosity by then. He would have spurred the' \
+       ' pony faster, but hisfather made them dismount beside the bridge and approach on foot. Bran jumped offand ran.By ' \
+       'then Jon, Jory, and Theon Greyjoy had all dismounted as well. â€œWhat in the sevenhells is it?â€ Greyjoy was' \
+       ' saying.â€œA wolf,â€ Robb told him.â€œA freak,â€ Greyjoy said. â€œLook at the size of it.â€ ' \
+       'Branâ€™s heart was thumping in his chest as he pushed through a waist-high drift to hisbrothersâ€™ side.' \
+       'Half-buried in bloodstained snow, a huge dark shape slumped in death. Ice had formedin its shaggy grey fur,' \
+       ' and the faint smell of corruption clung to it like a womanâ€™sperfume. Bran glimpsed blind eyes crawling with' \
+       ' maggots, a wide mouth full of yellowedteeth. But it was the size of it that made him gasp. It was bigger than ' \
+       'his pony, twice thesize of the largest hound in his fatherâ€™s kennel.â€œItâ€™s no freak,â€ Jon said calmly. ' \
+       'â€œThatâ€™s a direwolf. They grow larger than the other kind.â€Theon Greyjoy said, â€œThereâ€™s not been a ' \
+       'direwolf sighted south of the Wall in twohundred years.â€â€œI see one now,â€ Jon replied.Bran tore his eyes ' \
+       'away from the monster. That was when he noticed the bundle inRobbâ€™s arms. He gave a cry of delight and moved ' \
+       'closer. The pup was a tiny ball of greyblack fur, its eyes still closed. It nuzzled blindly against Robbâ€™s ' \
+       'chest as he cradled it,searching for milk among his leathers, making a sad little whimpery sound. Branreached ' \
+       'out hesitantly. â€œGo on,â€ Robb told him. â€œYou can touch him.â€Bran gave the pup a quick nervous stroke, ' \
+       'then turned as Jon said, â€œHere you go.â€ Hishalf brother put a second pup into his arms. â€œThere are five ' \
+       'of them.â€Bran sat down inthe snow and hugged the wolf pup to his face. Its fur was soft and warm against ' \
+       'hischeek.â€œDirewolves loose in the realm, after so many years,â€ muttered Hullen, the master ofhorse. â€œI ' \
+       'like it not.â€â€œIt is a sign,â€ Jory said.Father frowned. â€œThis is only a dead animal, Jory,â€ he said. ' \
+       'Yet he seemed troubled.Snow crunched under his boots as he moved around the body. â€œDo we know what killed ' \
+       'her?â€â€œThereâ€™s something in the throat,â€ Robb told him, proud to have found the answer beforehis father' \
+       ' even asked. â€œThere, just under the jaw.â€His father knelt and groped under the beastâ€™s head with his hand.' \
+       ' He gave a yank andheld it up for all to see. A foot of shattered antler, tines snapped off, all wet with blood. ' \
+       'A sudden silence descended over the party. The men looked at the antler uneasily, andno one dared to speak. ' \
+       'Even Bran could sense their fear, though he did not understand. His father tossed the antler to the side and ' \
+       'cleansed his hands in the snow. â€œIâ€™msurprised she lived long enough to whelp,â€ he said. His voice broke ' \
+       'the spell.â€œMaybe she didnâ€™t,â€ Jory said. â€œIâ€™ve heard tales . . . maybe the bitch was already deadwhen ' \
+       'the pups came.â€â€œBorn with the dead,â€ another man put in. â€œWorse luck.â€â€œNo matter,â€ said Hullen.' \
+       ' â€œThey be dead soon enough too.â€Bran gave a wordless cry of dismay.â€œThe sooner the better,â€ Theon' \
+       ' Greyjoy agreed. He drew his sword. â€œGive the beast here,Bran.â€The little thing squirmed against him, as ' \
+       'if it heard and understood. â€œNo!â€ Bran criedout fiercely. â€œItâ€™s mine.â€â€œPut away your sword, ' \
+       'Greyjoy,â€ Robb said. For a moment he sounded as commandingas their father, like the lord he would someday be.' \
+       ' â€œWe will keep these pups.â€â€œYou cannot do that, boy,â€ said Harwin, who was Hullenâ€™s son.â€œIt be a ' \
+       'mercy to kill them,â€ Hullen said.Bran looked to his lord father for rescue, but got only a frown, a furrowed ' \
+       'brow. â€œHullenspeaks truly, son. Better a swift death than a hard one from cold and starvation.â€â€œNo!â€' \
+       ' He could feel tears welling in his eyes, and he looked away. He did not want to cryin front of his father.Robb' \
+       ' resisted stubbornly. â€œSer Rodrikâ€™s red bitch whelped again last week,â€ he said. â€œItwas a small litter,' \
+       ' only two live pups. Sheâ€™ll have milk enough.â€â€œSheâ€™ll rip them apart when they try to nurse.â€â€œLord ' \
+       'Stark,â€ Jon said. It was strange to hear him call Father that, so formal. Branlooked at him with desperate hope.' \
+       ' â€œThere are five pups,â€ he told Father. â€œThree male,two female.â€â€œWhat of it, Jon?â€â€œYou have five ' \
+       'trueborn children,â€ Jon said. â€œThree sons, two daughters. The direwolf isthe sigil of your House. Your children' \
+       ' were meant to have these pups, my lord.â€Bran saw his fatherâ€™s face change, saw the other men exchange ' \
+       'glances. He loved Jonwith all his heart at that moment. Even at seven, Bran understood what his brother haddone.' \
+       ' The count had come right only because Jon had omitted himself. He had includedthe girls, included even Rickon,' \
+       ' the baby, but not the bastard who bore the surnameSnow, the name that custom decreed be given to all those in the' \
+       ' north unlucky enough tobe born with no name of their own. Their father understood as well. â€œYou want no pup ' \
+       'for yourself, Jon?â€he asked softly.â€œThe direwolf graces the banners of House Stark,â€ Jon pointed out.' \
+       ' â€œI am no Stark,Father.â€Their lord father regarded Jon thoughtfully. Robb rushed into the silence he left.' \
+       ' â€œI willnurse him myself, Father,â€ he promised. â€œI will soak a towel with warm milk, and givehim suck from ' \
+       'that.â€â€œMe too!â€ Bran echoed.The lord weighed his sons long and carefully with his eyes. â€œEasy to say,' \
+       ' and harder todo. I will not have you wasting the servantsâ€™ time with this. If you want these pups, you will ' \
+       'feed them yourselves. Is that understood?â€Bran nodded eagerly. The pup squirmed in his grasp, licked at his' \
+       ' face with a warmtongue.â€œYou must train them as well,â€ their father said. â€œYou must train them. The ' \
+       'kennel master will have nothing to do with these monsters, I promise you that. And thegods help you if you ' \
+       'neglect them, or brutalize them, or train them badly. These are notdogs to beg for treats and slink off at a ' \
+       'kick. A direwolf will rip a manâ€™s arm off hisshoulder as easily as a dog will kill a rat. Are you sure you' \
+       ' want this?â€â€œYes, Father,â€ Bran said.â€œYes,â€ Robb agreed.â€œThe pups may die anyway, despite all you ' \
+       'do.â€â€œThey wonâ€™t die,â€ Robb said. â€œWe wonâ€™t let them die.â€â€œKeep them, then. Jory, Desmond, ' \
+       'gather up the other pups. Itâ€™s time we were back toWinterfell.â€It was not until they were mounted and on ' \
+       'their way that Bran allowed himself to tastethe sweet air of victory. By then, his pup was snuggled inside his' \
+       ' leathers, warm againsthim, safe for the long ride home. Bran was wondering what to name him.Halfway across the' \
+       ' bridge, Jon pulled up suddenly.â€œWhat is it, Jon?â€ their lord father asked.â€œCanâ€™t you hear it?â€Bran ' \
+       'could hear the wind in the trees, the clatter of their hooves on the ironwood planks,the whimpering of his ' \
+       'hungry pup, but Jon was listening to something else.â€œThere,â€ Jon said. He swung his horse around and ' \
+       'galloped back across the bridge. Theywatched him dismount where the direwolf lay dead in the snow, watched him' \
+       ' kneel. Amoment later he was riding back to them, smiling.â€œHe must have crawled away from the others,â€ ' \
+       'Jon said. â€œOr been driven away,â€ their father said, looking at the sixth pup. His fur was white,where the' \
+       ' rest of the litter was grey. His eyes were as red as the blood of the ragged manwho had died that morning. ' \
+       'Bran thought it curious that this pup alone would haveopened his eyes while the others were still blind.â€œAn' \
+       ' albino,â€ Theon Greyjoy said with wry amusement. â€œThis one will die even faster thanthe others.â€Jon Snow' \
+       ' gave his fatherâ€™s ward a long, chilling look. â€œI think not, Greyjoy,â€ he said.â€œThis one belongs to me' \
+       '.â€'
