c75aa7984d6dbe7a2833bd1ce24f177d9e389ee8
MadeleineHarbom <madeleine.harbom@gmail.com>
2020-06-09 23:21:10 +0200

    23123123

--- L16_traveling_salesman/branch_bound.py
+++ L16_traveling_salesman/branch_bound.py
@@ -1,20 +1,15 @@
 
 from L16_traveling_salesman.adaptable_heap_priority_queue import *
+from L16_traveling_salesman.travels import *
 import urllib.request
 
-def pretty_print(sudoku):
-    for i in range(9):
-        if i % 3 == 0:
-            print('')
-        s = ''
-        for j in range (9):
-            if j % 3 == 0:
-                s += '   '
+def pretty_print(matrixx):
+    for i in range(len(matrixx)):
 
-            else:
-                s += (' ')
-
-            s += str(sudoku[i][j])
+        s = '\n'
+        for j in range(len(matrixx[i])):
+            s += '   '
+            s += str(matrixx[i][j])
 
         print(s)
 
@@ -29,7 +24,8 @@ def import_tsp():
         dist.append(d)
     return dist
 
-def create_matrix(matriks):
+
+def inf_matrix(matriks):
     for i in range(len(matriks)):
         for j in range(len(matriks)):
             if matriks[i][j] == 0:
@@ -59,22 +55,31 @@ def reduce(matriks):
         for k in range(len(matriks[i])):
             matriks[k][i] -= min_col
         total += min_col
+    pretty_print(matriks)
+    print('----------------')
+    #inf_matrix(matriks)
+    pretty_print(matriks)
+    print('===================')
     return total
 
 
-def set_inf(matriks, row, col):
+def set_inf_reduce(matriks, row, col):
     matriks[col][row] = float('inf')
     for i in range(len(matriks)):
+        #print(matriks[i][col])
+        #print(matriks[row][i])
         matriks[i][col] = float('inf')
         matriks[row][i] = float('inf')
+
     return matriks
 
 
 def find_travel(matriks):
-    m = create_matrix(matriks)
+    m = inf_matrix(matriks)
     r = reduce(m)
     q = AdaptableHeapPriorityQueue() #hvorfor en priority queue?
     q.add(r, (m, 0)) #mængden den blevet recuderet er key, til en tuple med (matrix, level)
+    cost = 0
     while not q.is_empty():
         key, value = q.remove_min() #går nu den korterste vej
         #remove min returneret (item._key, item._value)
@@ -83,19 +88,27 @@ def find_travel(matriks):
             if matriks[level][j] == float('inf'):
                 continue #j++
             temp_m = matrixx.copy() #ikke ændre på orginal matrix hvis shit går galt
-            set_inf(temp_m, level, j)
+            print('Level (uninfed ' + str(level))
+            pretty_print(matrixx)
+            temp_m = set_inf_reduce(temp_m, level, j)
+            print('Level (infed) ' + str(level))
+            pretty_print(temp_m)
             reduction = reduce(temp_m)
-            cost = key + matrixx[level][j] + reduction
+            cost = key + reduction
+            #costie = key + matrixx[level][j] + reduction
+            print(cost)
             q.add(cost, (temp_m, j)) #bygger træet
+        print(cost)
 
-    return m
+    return (m, cost)
 
 
 
 imported = import_tsp()
-pretty_print(imported)
+pretty_print(dist2)
 print('O')
 print('  ===============o')
 print('O')
-tsm_matrix = find_travel(imported)
+tsm_matrix, cost = find_travel(dist2)
 pretty_print(tsm_matrix)
+print(cost)
